ff12543a5ce9ddba0aad60908ca7f7c733cdc6cc
xulion <xulion.xulion@gmail.com>
2017-11-20 00:04:03 -0500

    refactored cscope command executor (#4)

---
+++ src/CscopeExecutor.ts
@@ -0,0 +1,117 @@
+'use strict';
+
+/*
+cscope find command:
+ 0 Find this C symbol:
+ 1 Find this function definition:
+ 2 Find functions called by this function:
+ 3 Find functions calling this function:
+ 4 Find this text string:
+ 5 Change this text string:
+ 6 Find this egrep pattern:
+ 7 Find this file:
+ 8 Find files #including this file:
+*/
+
+const spawnSync = require('child_process').spawnSync;
+const fs = require('fs');
+import SymbolLocation from './SymbolLocation';
+
+export default class CscopeExecutor {
+    source_paths:string[];
+    exec_path:string;
+
+    constructor(source_paths:string[], exec_path:string)
+    {
+        this.source_paths = source_paths;
+        this.exec_path = exec_path;
+    }
+
+    public buildDataBase():boolean{
+        let start = true;
+        this.source_paths.forEach((path) => {
+            const execConfig = {
+                cwd: this.exec_path,
+                env: process.env};
+    
+            let ret = spawnSync("mkdir", ['-p', 'cscope'], execConfig);
+            ret = spawnSync("find", [path, '-type', 'f', '-name', '*.c', 
+                               '-o', '-type', 'f', '-name', '*.h', 
+                               '-o', '-type', 'f', '-name', '*.cpp', 
+                               '-o', '-type', 'f', '-name', '*.cc', 
+                               '-o', '-type', 'f', '-name', '*.mm'], execConfig);
+            if (ret.stderr.length > 0) {
+                console.log(ret.stderr.toString());
+            }
+            else {
+                if (start) {
+                    fs.writeFileSync(this.exec_path + '/cscope/cscope.files', ret.stdout.toString());
+                }
+                else{
+                    fs.appendFileSync(this.exec_path + '/cscope/cscope.files', ret.stdout.toString());
+                }
+                start = false;
+            }
+        });
+    
+        const cscopeExecConfig = {
+            cwd: this.exec_path + '/cscope',
+            env: process.env};
+        const ret = spawnSync("cscope", ['-b', '-q', '-k'], cscopeExecConfig);
+        return true;
+    }
+
+    private execCommand(targetText:string, level:number):SymbolLocation[]{
+        const cscopeExecConfig = {
+            cwd: this.exec_path + '/cscope',
+            env: process.env};
+
+        let ret = spawnSync("cscope", ['-L' + level + targetText], cscopeExecConfig);
+        const fileList = ret.stdout.toString().split('\n');
+        let list = [];
+        fileList.forEach((line) =>{
+            const contents = line.split(' ');
+            if (contents.length > 3)
+            {
+                let fileName = contents[0];
+                console.log(fileName);
+                const lineNum = parseInt(contents[2]) - 1;
+                list.push(new SymbolLocation(fileName, lineNum, 0, 0));
+            }
+        });
+
+        return list;
+    }
+
+    public findReferences(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 0);
+    }
+
+    public findDefinition(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 1);
+    }
+
+    findCallee(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 2);
+    }
+
+    findCaller(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 3);
+    }
+
+    findText(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 4);
+    }
+
+    findPattern(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 6);
+    }
+
+    findThisFile(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 7);
+    }
+
+    findIncluder(symbol:string):SymbolLocation[]{
+        return this.execCommand(symbol, 8);
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ const spawnSync = require('child_process').spawnSync;
 const fs = require('fs');
 import {RefProvider} from './RefProvider';
 import {DefinitionProvider} from './DefinitionProvider';
+import CscopeExecutor from './CscopeExecutor';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -22,8 +23,10 @@ export function activate(context: vscode.ExtensionContext) {
         buildDataBase();
     });
 
-    context.subscriptions.push(vscode.languages.registerReferenceProvider(["cpp", "c"], new RefProvider()));
-    context.subscriptions.push(vscode.languages.registerDefinitionProvider(['cpp', 'c'], new DefinitionProvider()));
+    const executor = new CscopeExecutor(null, vscode.workspace.rootPath + '/.vscode');
+
+    context.subscriptions.push(vscode.languages.registerReferenceProvider(["cpp", "c"], new RefProvider(executor)));
+    context.subscriptions.push(vscode.languages.registerDefinitionProvider(['cpp', 'c'], new DefinitionProvider(executor)));
     
 }
 
@@ -62,44 +65,24 @@ function buildDataBase()
     const sourcePaths = configurations.configurations[0].cscope.paths;
 
     // start with linux command line since this is easier. Later shall change
-    // to node api for file search.
+    // to node api for file search.5
     // Now we are building the database
     vscode.window.showInformationMessage('Building cscope database!');
 
-    let start = true;
+    const execConfig = {
+        cwd: vscode.workspace.rootPath,
+        env: process.env};
+    let ret = spawnSync("mkdir", ['-p', '.vscode'], execConfig);
 
+    let paths = [];
     sourcePaths.forEach((path) => {
         const fullPath = path.replace("${workspaceRoot}", vscode.workspace.rootPath);
-        const execConfig = {
-            cwd: vscode.workspace.rootPath,
-            env: process.env};
-
-        let ret = spawnSync("mkdir", ['-p', '.vscode'], execConfig);
-        spawnSync("mkdir", ['-p', '.vscode/cscope'], execConfig);
-        ret = spawnSync("find", [fullPath, '-type', 'f', '-name', '*.c', 
-                           '-o', '-type', 'f', '-name', '*.h', 
-                           '-o', '-type', 'f', '-name', '*.cpp', 
-                           '-o', '-type', 'f', '-name', '*.cc', 
-                           '-o', '-type', 'f', '-name', '*.mm'], execConfig);
-        if (ret.stderr.length > 0)
-        {
-            console.log(ret.stderr.toString());
-        }
-        else{
-            if (start){
-                fs.writeFileSync(vscode.workspace.rootPath + '/.vscode/cscope/cscope.files', ret.stdout.toString());
-            }
-            else{
-                fs.appendFileSync(vscode.workspace.rootPath + '/.vscode/cscope/cscope.files', ret.stdout.toString());
-            }
-            start = false;
-        }
+        paths.push(fullPath);
     });
 
-    const cscopeExecConfig = {
-        cwd: vscode.workspace.rootPath + '/.vscode/cscope',
-        env: process.env};
-    const ret = spawnSync("cscope", ['-b', '-q', '-k'], cscopeExecConfig);
+    const executor = new CscopeExecutor(paths, vscode.workspace.rootPath + '/.vscode');
+    executor.buildDataBase();
+
     vscode.window.showInformationMessage('Building finished!');
 }
 
